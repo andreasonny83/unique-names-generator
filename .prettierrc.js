c0fa700df9bdd84b25fc2ee9420796356d6413e9
andreasonny83 <andreasonny83@gmail.com>
2019-11-12 17:27:07 +0000

    chore(core): Refactor folder structure
    * Add Microbundle for module creation. This will allow adding support
    for umd and tree-shaking

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  tabWidth: 2,
+  useTabs: false,
+  parser: 'typescript',
+};
