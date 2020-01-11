2bb1a9e45b1edb583abd918acf600fc1ba50756d
Grant Blakeman <hello@grantblakeman.com>
2020-01-10 17:42:23 -0800

    fix(dictionaries): add “countries” to import/export list

--- src/dictionaries/index.ts
+++ src/dictionaries/index.ts
@@ -1,7 +1,8 @@
 import adjectives from './adjectives';
 import animals from './animals';
 import colors from './colors';
+import countries from './countries';
 import names from './names';
 import starWars from './star-wars';
 
-export { adjectives, animals, colors, names, starWars };
+export { adjectives, animals, colors, countries, names, starWars };
